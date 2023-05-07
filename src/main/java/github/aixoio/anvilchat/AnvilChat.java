89b11a2644a81e1ddf977ed87d40e0360e56a156
aixoio <79969530+aixoio@users.noreply.github.com>
2023-05-07 15:00:36 +1000

    Init

---
+++ src/main/java/github/aixoio/anvilchat/AnvilChat.java
@@ -0,0 +1,17 @@
+package github.aixoio.anvilchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AnvilChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
